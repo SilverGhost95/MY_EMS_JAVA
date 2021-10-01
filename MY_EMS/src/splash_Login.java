e065ee08b7f8780264a20d0adbfa346392f67472
SilverGhost95 <ricogentles@gmail.com>
2021-09-30 19:56:16 -0500

    Splash colors decided

--- MY_EMS/src/splash_Login.java
+++ MY_EMS/src/splash_Login.java
@@ -1,6 +1,8 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Color;
 
 public class splash_Login {
 
@@ -37,6 +39,17 @@ public class splash_Login {
 		frame.setUndecorated(true);
 		frame.setBounds(100, 100, 900, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(244, 180, 26));
+		panel.setForeground(Color.WHITE);
+		panel.setBounds(0, 0, 458, 500);
+		frame.getContentPane().add(panel);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(20, 61, 89));
+		panel_1.setBounds(455, 0, 458, 500);
+		frame.getContentPane().add(panel_1);
 	}
-
 }

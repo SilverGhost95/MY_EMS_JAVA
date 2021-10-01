0f43f45d7c07868b83bf6e44587a23467d8afcf2
SilverGhost95 <ricogentles@gmail.com>
2021-09-30 20:33:26 -0500

    Added "Hominid Logo"

--- MY_EMS/src/splash_Login.java
+++ MY_EMS/src/splash_Login.java
@@ -1,8 +1,12 @@
 import java.awt.EventQueue;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.border.LineBorder;
 
 public class splash_Login {
 
@@ -41,15 +45,29 @@ public class splash_Login {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JPanel panel = new JPanel();
-		panel.setBackground(new Color(244, 180, 26));
-		panel.setForeground(Color.WHITE);
-		panel.setBounds(0, 0, 458, 500);
-		frame.getContentPane().add(panel);
+		JPanel panelSplash = new JPanel();
+		panelSplash.setBackground(new Color(244, 180, 26));
+		panelSplash.setForeground(Color.WHITE);
+		panelSplash.setBounds(0, 0, 458, 500);
+		frame.getContentPane().add(panelSplash);
+		panelSplash.setLayout(null);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(new Color(20, 61, 89));
-		panel_1.setBounds(455, 0, 458, 500);
-		frame.getContentPane().add(panel_1);
+		JLabel lblLogo = new JLabel("");
+		lblLogo.setBorder(null);
+		lblLogo.setBounds(116, 152, 211, 187);
+		panelSplash.add(lblLogo);
+		ImageIcon icon = new ImageIcon("C:\\Users\\ricog\\git\\MY_EMS_JAVA\\MY_EMS\\icons\\logo.png");
+		//scale image to fit label
+		Image img = icon.getImage();
+		Image imgScale = img.getScaledInstance(lblLogo.getWidth(), lblLogo.getHeight(), Image.SCALE_SMOOTH);
+		ImageIcon scaledIcon = new ImageIcon(imgScale);
+		lblLogo.setIcon(scaledIcon);
+		
+		
+		JPanel panelLogin = new JPanel();
+		panelLogin.setBackground(new Color(20, 61, 89));
+		panelLogin.setBounds(455, 0, 458, 500);
+		frame.getContentPane().add(panelLogin);
+		panelLogin.setLayout(null);
 	}
 }

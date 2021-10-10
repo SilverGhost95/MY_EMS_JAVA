cc9ea8317fa174752b0c808e0ff43cd8722141f8
SilverGhost95 <ricogentles@gmail.com>
2021-10-09 20:01:04 -0500

    HOME UI

--- MY_EMS/src/emsHome.java
+++ MY_EMS/src/emsHome.java
@@ -1,6 +1,11 @@
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class emsHome {
 
@@ -34,9 +39,32 @@ public class emsHome {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setUndecorated(true);
-		frame.setBounds(100, 100, 1136, 700);
+		frame.setLocationRelativeTo(null);
+		frame.setBounds(100, 100, 1408, 936);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+
+		JPanel pnlMenuleft = new JPanel();
+		pnlMenuleft.setBounds(0, 63, 230, 846);
+		pnlMenuleft.setBackground(new Color(0, 23, 48));
+		frame.getContentPane().add(pnlMenuleft);
+		pnlMenuleft.setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(0, 110, 230, 58);
+		panel_1.setBackground(new Color(0, 23, 48));
+		pnlMenuleft.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBounds(10, 10, 44, 48);
+		panel_1.add(lblNewLabel);
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\ricog\\git\\MY_EMS_JAVA\\MY_EMS\\icons\\outline_home_white_36dp.png"));
+
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 1394, 63);
+		panel.setBackground(new Color(20, 61, 89));
+		frame.getContentPane().add(panel);
 	}
 
 }
--- MY_EMS/src/splash_Login.java
+++ MY_EMS/src/splash_Login.java
@@ -1,28 +1,20 @@
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.Color;
 import javax.swing.JLabel;
-import javax.swing.border.LineBorder;
-import javax.swing.JTextField;
+import javax.swing.JPanel;
 import javax.swing.JPasswordField;
-import java.awt.Font;
-import java.awt.Button;
-import javax.swing.JButton;
-import javax.swing.JTextPane;
-import javax.swing.JToolBar.Separator;
 import javax.swing.JSeparator;
-import java.awt.event.MouseMotionAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.FocusAdapter;
-import java.awt.event.FocusEvent;
-import java.awt.Cursor;
-import javax.swing.UIManager;
-import java.awt.event.MouseAdapter;
+import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
 
 public class splash_Login {
 
@@ -63,41 +55,37 @@ public class splash_Login {
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		JPanel panelSplash = new JPanel();
 		panelSplash.setBackground(new Color(244, 180, 26));
 		panelSplash.setForeground(Color.WHITE);
 		panelSplash.setBounds(0, 0, 458, 500);
 		frame.getContentPane().add(panelSplash);
 		panelSplash.setLayout(null);
-		
+
 		JLabel lblLogo = new JLabel("");
 		lblLogo.setBorder(null);
 		lblLogo.setBounds(116, 152, 211, 187);
 		panelSplash.add(lblLogo);
 		ImageIcon iconLogo = new ImageIcon("C:\\Users\\ricog\\git\\MY_EMS_JAVA\\MY_EMS\\icons\\logo.png");
-		lblLogo.setIcon(scaleIcon(lblLogo,iconLogo));
-		
-		
+		lblLogo.setIcon(scaleIcon(lblLogo, iconLogo));
+
 		JPanel panelLogin = new JPanel();
 		panelLogin.setBackground(new Color(20, 61, 89));
 		panelLogin.setBounds(455, 0, 445, 500);
 		frame.getContentPane().add(panelLogin);
 		panelLogin.setLayout(null);
-		
-		
+
 		JSeparator sptrUsername = new JSeparator();
 		sptrUsername.setBounds(101, 177, 290, 10);
 		panelLogin.add(sptrUsername);
-		
+
 		JSeparator sptrPassword = new JSeparator();
 		sptrPassword.setForeground(Color.LIGHT_GRAY);
 		sptrPassword.setBackground(Color.WHITE);
 		sptrPassword.setBounds(101, 258, 290, 10);
 		panelLogin.add(sptrPassword);
-		
 
-		
 		txtUsername = new JTextField();
 		txtUsername.setBorder(null);
 		txtUsername.setCaretColor(Color.WHITE);
@@ -108,7 +96,7 @@ public class splash_Login {
 		txtUsername.setBounds(101, 139, 290, 39);
 		panelLogin.add(txtUsername);
 		txtUsername.setColumns(10);
-		
+
 		pwdPassword = new JPasswordField();
 		pwdPassword.setBorder(null);
 		pwdPassword.setCaretColor(Color.WHITE);
@@ -118,13 +106,14 @@ public class splash_Login {
 		pwdPassword.setBackground(new Color(20, 61, 89));
 		pwdPassword.setBounds(101, 220, 290, 39);
 		panelLogin.add(pwdPassword);
-		
+
 		Button btnSignIn = new Button("Sign In");
 		btnSignIn.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				btnSignIn.setForeground(new Color(244, 180, 26));
 			}
+
 			@Override
 			public void mouseExited(MouseEvent e) {
 				btnSignIn.setForeground(Color.WHITE);
@@ -135,16 +124,17 @@ public class splash_Login {
 		btnSignIn.setFont(new Font("SansSerif", Font.PLAIN, 18));
 		btnSignIn.setForeground(Color.WHITE);
 		btnSignIn.setBackground(new Color(20, 61, 89));
-		//btnNewButton.setBorder(BorderFactory.createEmptyBorder());
+		// btnNewButton.setBorder(BorderFactory.createEmptyBorder());
 		btnSignIn.setBounds(101, 300, 123, 40);
 		panelLogin.add(btnSignIn);
-		
+
 		Button btnRegister = new Button("Register");
 		btnRegister.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				btnRegister.setForeground(new Color(244, 180, 26));
 			}
+
 			@Override
 			public void mouseExited(MouseEvent e) {
 				btnRegister.setForeground(Color.WHITE);
@@ -156,13 +146,14 @@ public class splash_Login {
 		btnRegister.setBackground(new Color(20, 61, 89));
 		btnRegister.setBounds(268, 300, 123, 40);
 		panelLogin.add(btnRegister);
-		
+
 		JLabel lblForgotPass = new JLabel("Forgot Password?");
 		lblForgotPass.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				lblForgotPass.setForeground(new Color(244, 180, 26));
 			}
+
 			@Override
 			public void mouseExited(MouseEvent e) {
 				lblForgotPass.setForeground(Color.WHITE);
@@ -174,44 +165,46 @@ public class splash_Login {
 		lblForgotPass.setForeground(Color.WHITE);
 		lblForgotPass.setBounds(179, 390, 135, 26);
 		panelLogin.add(lblForgotPass);
-		
+
 		JLabel lblUsername = new JLabel("");
 		lblUsername.setBounds(65, 144, 30, 30);
 		panelLogin.add(lblUsername);
 		ImageIcon iconUser = new ImageIcon("C:\\Users\\ricog\\git\\MY_EMS_JAVA\\MY_EMS\\icons\\account.png");
 		lblUsername.setIcon(scaleIcon(lblUsername, iconUser));
-		
+
 		JLabel lblPassword = new JLabel("");
 		lblPassword.setBounds(65, 224, 30, 30);
 		panelLogin.add(lblPassword);
 		ImageIcon iconPassword = new ImageIcon("C:\\Users\\ricog\\git\\MY_EMS_JAVA\\MY_EMS\\icons\\lock.png");
-		lblPassword.setIcon(scaleIcon(lblPassword,iconPassword));
-		
+		lblPassword.setIcon(scaleIcon(lblPassword, iconPassword));
+
 		JLabel lblExit = new JLabel("");
 		lblExit.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				lblExit.setBorder(new LineBorder(Color.WHITE, 1, true));
 			}
+
 			@Override
 			public void mouseExited(MouseEvent e) {
 				lblExit.setBorder(null);
 			}
+
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				 frame.dispose();
+				frame.dispose();
 			}
 		});
 		lblExit.setBounds(394, 23, 30, 26);
 		panelLogin.add(lblExit);
 		ImageIcon iconExit = new ImageIcon("C:\\Users\\ricog\\git\\MY_EMS_JAVA\\MY_EMS\\icons\\exit.png");
-		lblExit.setIcon(scaleIcon(lblExit,iconExit));
+		lblExit.setIcon(scaleIcon(lblExit, iconExit));
 
 	}
-	
-	//Allows icons to fit into label sizes
+
+	// Allows icons to fit into label sizes
 	private ImageIcon scaleIcon(JLabel label, ImageIcon icon) {
-		
+
 		Image img = icon.getImage();
 		Image imgScaled = img.getScaledInstance(label.getWidth(), label.getHeight(), Image.SCALE_SMOOTH);
 		ImageIcon iconScaled = new ImageIcon(imgScaled);
